990b8bb58739bb6ae4690231dbb92deb3b3d1d26
Aman Varshney <amanvarshney.work@gmail.com>
2025-09-27 10:55:31 +0530

    commit env

--- packages/auth/src/index.ts
+++ packages/auth/src/index.ts
@@ -1,23 +1,23 @@
-import { betterAuth, type BetterAuthOptions } from "better-auth";
-import { drizzleAdapter } from "better-auth/adapters/drizzle";
-import { db } from "@my-better-t-app/db";
+import { db } from "@my-better-t-app/db/index";
 import * as schema from "@my-better-t-app/db/schema/auth";
+import { type BetterAuthOptions, betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
 
 export const auth = betterAuth<BetterAuthOptions>({
-    database: drizzleAdapter(db, {
-        provider: "sqlite",
+	database: drizzleAdapter(db, {
+		provider: "sqlite",
 
-        schema: schema,
-    }),
-    trustedOrigins: [process.env.CORS_ORIGIN || ""],
-    emailAndPassword: {
-        enabled: true,
-    },
-    advanced: {
-        defaultCookieAttributes: {
-            sameSite: "none",
-            secure: true,
-            httpOnly: true,
-        },
-    },
+		schema: schema,
+	}),
+	trustedOrigins: [process.env.CORS_ORIGIN || ""],
+	emailAndPassword: {
+		enabled: true,
+	},
+	advanced: {
+		defaultCookieAttributes: {
+			sameSite: "none",
+			secure: true,
+			httpOnly: true,
+		},
+	},
 });
